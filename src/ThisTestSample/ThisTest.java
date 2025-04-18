af470890a6a823ab1a8b8727715a9f51c96d2b3a
Ashwini <ashwiniswami1995@gmail.com>
2025-04-19 00:44:13 +0530

    Initial commit

---
+++ src/ThisTestSample/ThisTest.java
@@ -0,0 +1,29 @@
+package ThisTestSample;
+
+public class ThisTest {
+	 int number;
+	 String name;
+
+	ThisTest(int number, String name) {
+		this();
+		this.number = number;
+		this.name = name;// 'this' refers to the instance variable
+		System.out.println("under constructor  "+name +"  "+number);
+	}
+
+	ThisTest() {
+		System.out.println("default constructor");
+
+	}
+
+	public static void main(String[] args) {
+
+		ThisTest obj = new ThisTest(10, "ash");
+		System.out.println(obj.name + " " + obj.number);
+		ThisTest obj1 = new ThisTest(11, "atish");
+		System.out.println(obj1.name + " " + obj1.number);
+
+	}
+}
+
+
